90491cc029cc128994d50bc5803db633e9570aee
andy-yk-lin <andy_yk_lin@gss.com.tw>
2024-11-29 11:39:56 +0800

    Update MendSCAScan.groovy

--- vars/MendSCAScan.groovy
+++ vars/MendSCAScan.groovy
@@ -9,7 +9,7 @@ def call(Map args = [:]) {
         export repo=\$(basename -s .git \$(git config --get remote.origin.url))
         export branch=\$(git rev-parse --abbrev-ref HEAD)
 
-        ./mend dep -u ${reachabilityFlag} -s "*//\${JOB_NAME}//\${repo}_\${branch}" --fail-policy --non-interactive --export-results dep-results.txt
+        ./mend dep -u ${reachabilityFlag} -s "/var/lib/jenkins/workspace/CLI_Test/helloWorld" --fail-policy --non-interactive --export-results dep-results.txt
         
         dep_exit=\$?
         if [[ "\$dep_exit" == "9" ]]; then
